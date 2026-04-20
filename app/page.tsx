060a41b480c7a7f7841af1aaffb70c16e69b2ef5
amdadisalm01 <amdadislam733@gmail.com>
2026-04-21 00:22:27 +0600

    MODIFIED home page to integrate About, Features, and News sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import HeroSection from "@/components/ui/HeroSection";
+import AboutSection from "@/components/ui/AboutSection";
+import FeaturesSection from "@/components/ui/FeaturesSection";
+import NewsSection from "@/components/ui/NewsSection";
 
 export default function Home() {
   return (
     <main className="flex-1 flex flex-col">
       <HeroSection />
+      <AboutSection />
+      <FeaturesSection />
+      <NewsSection />
     </main>
   );
 }
